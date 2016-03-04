5691689a985e7c2ef496de13ebb372374203b884
ab5y <ab5s10@gmail.com>
2016-03-04 22:11:07 +0530

    Initial Commit

---
+++ landing.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, session, g, redirect, url_for, \
+				abort, render_template, flash
+
+# Configuation
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def show_landing():
+	print 'Reaches here'
+	return render_template('landing.html')
+
+@app.route('/add_email', methods=['POST'])
+def add_email():
+	email = request.form['email']
+	flash('Your email was successfully stolen by the corporate machinery: '+email)
+	return redirect(url_for('show_landing'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_landing'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_landing'))
+
+if __name__ == '__main__':
+	app.run()
