9d89eabd9cba66ff758821c8dae27b05a3f58f87
ab5y <ab5s10@gmail.com>
2016-03-04 23:44:16 +0530

    fixed the problem

--- landing.py
+++ landing.py
@@ -12,33 +12,33 @@ app.config.from_object(__name__)
 @app.route('/')
 def show_landing():
 	print 'Reaches here'
-	return render_template('landing.html')
+	return render_template('show_landing.html')
 
-@app.route('/add_email', methods=['POST'])
-def add_email():
-	email = request.form['email']
-	flash('Your email was successfully stolen by the corporate machinery: '+email)
-	return redirect(url_for('show_landing'))
+# @app.route('/add_email', methods=['POST'])
+# def add_email():
+# 	email = request.form['email']
+# 	flash('Your email was successfully stolen by the corporate machinery: '+email)
+# 	return redirect(url_for('show_landing'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']:
-			error = 'Invalid username'
-		elif request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid password'
-		else:
-			session['logged_in'] = True
-			flash('You were logged in')
-			return redirect(url_for('show_landing'))
-	return render_template('login.html', error=error)
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+# 	error = None
+# 	if request.method == 'POST':
+# 		if request.form['username'] != app.config['USERNAME']:
+# 			error = 'Invalid username'
+# 		elif request.form['password'] != app.config['PASSWORD']:
+# 			error = 'Invalid password'
+# 		else:
+# 			session['logged_in'] = True
+# 			flash('You were logged in')
+# 			return redirect(url_for('show_landing'))
+# 	return render_template('login.html', error=error)
 
-@app.route('/logout')
-def logout():
-	session.pop('logged_in', None)
-	flash('You were logged out')
-	return redirect(url_for('show_landing'))
+# @app.route('/logout')
+# def logout():
+# 	session.pop('logged_in', None)
+# 	flash('You were logged out')
+# 	return redirect(url_for('show_landing'))
 
 if __name__ == '__main__':
 	app.run()
